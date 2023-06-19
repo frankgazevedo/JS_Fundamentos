11f39edf67bbb52697d8d682f6eebe3d860a7f59
Frank Azevedo <frankgazevedo@gmail.com>
2023-06-19 05:38:10 -0400

    JS: funções - Arrow Function

--- funcao/arrowFunction1.js
+++ funcao/arrowFunction1.js
@@ -1,18 +1,29 @@
+// function expression
 let dobro = function (a) {
     return 2 * a
 }
-
-dobro = (a) => {
-    return 2 * a 
-}
-
-dobro = a => 2 * a // return implícito
 console.log(dobro(Math.PI))
 
+// Toda Arrow Function é uma function expression
+dobro = (a) => {
+    return 2 * a // retorno EXPLÍCITP
+}
+console.log(dobro(Math.PI))
+
+// Toda Arrow Function é uma function expression
+dobro = a => 2 * a // retorno IMPLÍCITO
+console.log(dobro(Math.PI))
+
+// function expression
 let ola = function () {
     return 'Olá'
 }
+console.log(ola())
 
-ola = () => 'Olá'
-ola = _ => 'Olá' // possui um param
+// Toda Arrow Function é uma function expression
+ola = () => 'Olá' // retorno IMPLÍCITO
+console.log(ola())
+
+// Toda Arrow Function é uma function expression
+ola = _ => 'Olá' // possui um param // retorno IMPLÍCITO
 console.log(ola())
